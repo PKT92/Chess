d787b2d413c155b61d734af55a70479991924f76
PKT92 <preston.tran@gmail.com>
2019-06-19 10:32:44 -0600

    Tile is now a pointer, works better for Board code

--- Piece.cpp
+++ Piece.cpp
@@ -4,12 +4,12 @@
 // Piece(bool color, bool alive, int piece_type, int startx, int starty);
 // ~Piece();
 // void move_piece(int newx, int newy);
-Piece::Piece(): color{0}, alive{1}, piece{0}, tile{Tile()}{}
-Piece::Piece(bool start_color, int piece_type, Tile x)
+Piece::Piece(): color{0}, alive{1}, piece{0}, tile{nullptr}{}
+Piece::Piece(bool start_color, int piece_type, Tile *x)
 : color{start_color}, alive{1}, piece{piece_type}, tile{x}{
-    tile.set_piece(this);
+    tile->set_piece(this);
 }
-void Piece::move_piece(Tile x){tile = x;}
+void Piece::move_piece(Tile *x){tile->remove_piece(), tile = x, tile->set_piece(this);}
 int Piece::get_piece(){
     return piece;
 }
--- Piece.h
+++ Piece.h
@@ -1,18 +1,18 @@
 #pragma once
 #include "Tile.h"
-
+class Tile;
 class Piece
 {
 public:
     Piece();
-    Piece(bool start_color, int piece_type, Tile tile_num);
+    Piece(bool start_color, int piece_type, Tile *tile_num);
     ~Piece();
-    void move_piece(Tile x);
+    void move_piece(Tile *x);
     int get_piece();
 private:
     int piece; // 0 pawn, 1 knight, 2 bishop, 3 rook, 4 queen, 5 king
     int value; // 100 pawn, 300 bishop, 300 knight, 500 rook, 1000 queen, 5000 king
     bool alive; // 1 alive, 0 dead
     bool color; // 1 black, 0 white
-    Tile tile;
+    Tile *tile;
 };
